62e52040adcd05334dd4b2ee805402ef2f542f1f
Remco Hendriksen <hendriksenremco@hotmail.com>
2024-06-03 17:17:44 +0200

    Mail test

---
+++ mailtest.mjs
@@ -0,0 +1,27 @@
+import nodemailer from 'nodemailer'
+
+const transport = nodemailer.createTransport({
+  host: '172.22.1.253',
+  port: 588
+})
+
+transport.verify((error, success) => {
+  if (error) {
+    console.log(error)
+  } else {
+    console.log('Server is ready to take our messages')
+  }
+})
+
+// async function main () {
+//   const info = await transport.sendMail({
+//     from: 'website@hengeloserevue.nl',
+//     to: 'remco@remcohendriksen.nl',
+//     text: '<b>Helle world!</b>',
+//     subject: 'Test subject'
+//   })
+
+//   console.log('Message sent: %s', info.messageId)
+// }
+
+// main().catch(console.error)
