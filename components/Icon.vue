63a5c3fe7527ae4e287334d6b5200b57a047c37d
Remco Hendriksen <hendriksenremco@hotmail.com>
2024-06-05 23:34:05 +0200

    Dynamic import icon

--- components/Icon.vue
+++ components/Icon.vue
@@ -1,6 +1,6 @@
 <script setup>
-import { computed } from 'vue';
-import * as icons from "lucide-vue-next";
+import { computed } from 'vue'
+import * as icons from 'lucide-vue-next'
 
 const props = defineProps({
   name: {
@@ -12,15 +12,16 @@ const props = defineProps({
   strokeWidth: Number,
   defaultClass: String
 })
+const fileName = props.name.split(/\.?(?=[A-Z])/).join('-').toLowerCase()
 
-const icon = computed(() => icons[props.name]);
+const icon = await import(`~/node_modules/lucide-vue-next/dist/esm/icons/${fileName}.js`)
 </script>
 
 <template>
   <component
-    :is="icon"
+    :is="icon.default"
     :size="size"
     :color="color"
-    :stroke-width="strokeWidth" :default-class="defaultClass"
-  />
+    :stroke-width="strokeWidth"
+    :default-class="defaultClass" />
 </template>
