14d71650b5627e895a3530ea8c107505df72fe25
Remco Hendriksen <hendriksenremco@hotmail.com>
2024-06-06 15:15:54 +0200

    Sponsor scroller

--- components/Home/Sponsors.vue
+++ components/Home/Sponsors.vue
@@ -1,72 +1,61 @@
 <template>
   <div :class="$style['sponsors']">
     <div :class="$style['sponsors__wrapper']">
-      <div v-for="sponsor in items" :key="sponsor" :class="$style['sponsors__item']">
-        {{ sponsor.name }}
-      </div>
-    </div>
-    <div :class="$style['sponsors__wrapper']">
-      <div v-for="sponsor in items" :key="sponsor +'d'" :class="$style['sponsors__item']" aria-hidden="true">
-        {{ sponsor.name }}
-      </div>
+      <template v-for="sponsor in clonedStories" :key="sponsor">
+        <component :is="sponsor.content.link.cached_url ? nuxtLink : 'div'" :to="sponsor.content.link.cached_url ? sponsor.content.link.cached_url : undefined" :class="$style['sponsors__item']">
+          <NuxtImg v-if="sponsor.content.image.filename" :src="sponsor.content.image.filename" :alt="sponsor.content.name" :class="$style['sponsors__image']" />
+          <span v-else :class="$style['sponsors__name']">{{ sponsor.content.name }}</span>
+        </component>
+      </template>
     </div>
   </div>
 </template>
 <script setup>
+const nuxtLink = resolveComponent('nuxt-link')
+const { stories } = await useHrStoryblok('cdn/stories', { version: 'published', content_type: 'Sponsor' })
 
-const items = ref([
-  {
-    name: 'Jos & Rinus Paskamp'
-  },
-  {
-    name: 'Minie Lamers'
-  },
-  {
-    name: 'Ike en Gertie ter Stal'
-  },
-  {
-    name: 'Tanta Son'
-  },
-  {
-    name: 'A. van Rossum'
-  },
-  {
-    name: 'Iza fotografie'
-  },
-  {
-    name: 'Blankestijn advocaten mediators'
-  },
-  {
-    name: 'Erica bloemen & interieur'
-  },
-  {
-    name: 'Knoef druk en print'
-  },
-  {
-    name: 'Nijhof schoenen'
-  },
-  {
-    name: 'Bouwbedrijf Exterkate'
-  },
-  {
-    name: 'Jan Krul caravans & outdoor'
-  }
-])
+const sortedStories = stories.sort((a, b) => {
+  return a.content.image?.filename && !b.content.image?.filename ? -1 : 1
+})
+
+const clonedStories = [...sortedStories, ...sortedStories, ...sortedStories]
 </script>
 <style lang="scss" module>
 .sponsors {
   display: flex;
   gap: var(--spacing-xxl);
+  overflow: hidden;
 
   &__item {
-    aspect-ratio: 4/1;
+    aspect-ratio: 3/1;
     box-shadow: var(--box-shadow-elevation-2);
-    background-color: var(--opacity-surface);
-    display: inline-flex;
+    background-color: var(--text-base);
+    color: var(--background-base);
+    display: flex;
+    flex-direction: column;
     align-items: center;
     justify-content: center;
-    padding: var(--spacing-xxl);
+    opacity: 0.8;
     width: 100%;
+  }
+
+  &__image {
+    cursor: pointer;
+    object-fit: contain;
+    width: 100%;
+    height: 100%;
+    padding: var(--spacing);
+  }
+
+  &__name {
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-family: var(--font-family-heading);
+    padding: var(--spacing);
+    width: 100%;
+    height: 100%;
   }
 
   &__wrapper {
@@ -75,8 +64,9 @@ const items = ref([
     min-width: max-content;
     width:max-content;
     white-space: nowrap;
-    animation: marquee 100s linear infinite;
+    animation: marquee 50s linear infinite;
     will-change: transform;
+    transform: translateX(-33.333%)
   }
 
   &:hover {
@@ -88,11 +78,11 @@ const items = ref([
 
 @keyframes marquee {
   from {
-    transform: translateY(0)
+    transform: translateX(-33.333%)
   }
 
   to {
-    transform: translateX(calc(-100% - var(--spacing-xxl)));
+    transform: translateX(-66.66%);
   }
 }
 </style>
--- pages/styleguide.vue
+++ pages/styleguide.vue
@@ -1,12 +1,5 @@
 <template>
   <Container>
-    <Button>
-      Deploy test
-    </Button>
-    <Quote :image="'/bertus.jpg'">
-      Hallo<br>
-      Dit is een test
-    </Quote>
     <HomeSponsors />
   </Container>
 </template>
