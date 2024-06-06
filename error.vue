63fd6dff3c8fc70ed16293b23a3b61b2a025df19
Remco Hendriksen <hendriksenremco@hotmail.com>
2024-06-06 09:59:12 +0200

    Error page

---
+++ error.vue
@@ -0,0 +1,19 @@
+<template>
+  <Container :class="$style['error']" no-padding-top>
+    <Logo />
+    <h1>Oeps</h1>
+    <div>Er ging iets fout</div>
+    <Button transparent with-border class="h-margin-top-xl" icon="ArrowRight" icon-pos="right">
+      Breng me naar de homepage
+    </Button>
+  </Container>
+</template>
+<style lang="scss" module>
+.error {
+  display: flex;
+  height: 100dvh;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+</style>
--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -2,8 +2,16 @@
   <StoryblokComponent v-if="data?.story" :blok="data?.story.content" />
 </template>
 <script setup>
-const { slug } = useRoute().params
+const route = useRoute()
+const { slug } = route.params
 const url = slug && slug.length > 0 ? slug.join('/') : 'home'
-const data = await useHrStoryblok('cdn/stories/' + url.replace(/\/$/, ''))
+const data = ref(null)
 
+try {
+  data.value = await useHrStoryblok('cdn/stories/' + url.replace(/\/$/, ''))
+} catch (e) {
+  if (e.status === 404) {
+    navigateTo({ path: '/' }, { redirectCode: 301 })
+  }
+}
 </script>
